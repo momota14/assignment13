b5a4b712bb16a4c69f3d4628a848005880e8e7d3
Momota khatun <momotakhatun014@gmail.com>
2023-08-29 13:37:52 +0600

    fix issue

--- app/new-path/page.js
+++ app/new-path/page.js
@@ -1,32 +1,30 @@
-export async function fetchApi() {
-  const res = await fetch(`http://localhost:3000/api`);
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json();
+async function getData() {
+  const res = await fetch(
+    process.env.NODE_ENV === "development"
+      ? `http://localhost:3000/api`
+      : `https://assignment13-lime.vercel.app/api`
+  );
+  const repo = await res.json();
+  return { props: { repo } };
 }
 
-export default async function NewPage({ params }) {
-  console.log(params);
-
-  const data = await fetchApi();
-  console.log(data.message);
+export default async function NewPage({ repo }) {
+  const data = await getData();
+  console.log(data);
 
   return (
     <div className="px-10 py-5 flex flex-col justify-center break-words w-full">
       <div className="text-center text-2xl pb-3 font-semibold">
         This Data fetch from Api
       </div>
-      <div>{data.message}</div>
+      {/* <div>{repo?.message}</div>
       <div>
-        <span className="font-semibold">Your version is:</span> {data.version}
+        <span className="font-semibold">Your version is:</span> {repo?.version}
       </div>
       <div>
         <span className="font-semibold">Your token:</span>
-        {data.token}
-      </div>
+        {repo?.token}
+      </div> */}
     </div>
   );
 }
