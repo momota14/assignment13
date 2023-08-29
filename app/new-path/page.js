e83f33e193c0a19f6db6904bb0ef62b602b4214d
Momota khatun <momotakhatun014@gmail.com>
2023-08-29 14:05:19 +0600

    fix issue

--- app/new-path/page.js
+++ app/new-path/page.js
@@ -1,30 +1,45 @@
-async function getData() {
-  const res = await fetch(
-    process.env.NODE_ENV === "development"
-      ? `http://localhost:3000/api`
-      : `https://assignment13-lime.vercel.app/api`
-  );
-  const repo = await res.json();
-  return { props: { repo } };
-}
+"use client"
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-export default async function NewPage({ repo }) {
-  const data = await getData();
-  console.log(data);
+
+// export async function fetchApi() {
+//   const res = await fetch(
+//     process.env.NODE_ENV === "development"
+//       ? `http://localhost:3000/api`
+//       : `/api`
+//   );
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch data");
+//   }
+//   return res.json();
+// }
+
+const baseURL = process.env.NODE_ENV === "development" ? `http://localhost:3000/api` : `https://assignment13-lime.vercel.app/api`;
+
+export default  function NewPage() {
+  const [data, setPost] = useState(null);
+  useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(response.data);
+    });
+  }, [])
+
+  // console.log(data)
 
   return (
     <div className="px-10 py-5 flex flex-col justify-center break-words w-full">
       <div className="text-center text-2xl pb-3 font-semibold">
         This Data fetch from Api
       </div>
-      {/* <div>{repo?.message}</div>
+      <div>{data?.message}</div>
       <div>
-        <span className="font-semibold">Your version is:</span> {repo?.version}
+        <span className="font-semibold">Your version is:</span> {data?.version}
       </div>
       <div>
         <span className="font-semibold">Your token:</span>
-        {repo?.token}
-      </div> */}
+        {data?.token}
+      </div>
     </div>
   );
 }
