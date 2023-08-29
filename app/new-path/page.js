8ccb286360f95ae3859ea2597a804c37bc975c23
Momota khatun <momotakhatun014@gmail.com>
2023-08-29 13:02:21 +0600

    fix issue

--- app/new-path/page.js
+++ app/new-path/page.js
@@ -3,7 +3,7 @@ export async function fetchApi() {
    const res = await fetch(
      process.env.NODE_ENV === "development"
        ? `http://localhost:3000/api`
-       : `/api/products`
+       : `https://assignment13-lime.vercel.app/api/products`
    );
 
   if (!res.ok) {
