309558093102a59c5f778959d98dc33b1b683b36
Momota khatun <momotakhatun014@gmail.com>
2023-08-29 13:28:44 +0600

    check deploy

--- app/new-path/page.js
+++ app/new-path/page.js
@@ -1,10 +1,5 @@
 export async function fetchApi() {
-  // console.log(ctx)
-   const res = await fetch(
-     process.env.NODE_ENV === "development"
-       ? `http://localhost:3000/api`
-       : `https://assignment13-lime.vercel.app/api`
-   );
+  const res = await fetch(`http://localhost:3000/api`);
 
   if (!res.ok) {
     throw new Error("Failed to fetch data");
@@ -13,16 +8,25 @@ export async function fetchApi() {
   return res.json();
 }
 
-export default async function NewPage() {
+export default async function NewPage({ params }) {
+  console.log(params);
+
   const data = await fetchApi();
   console.log(data.message);
 
   return (
     <div className="px-10 py-5 flex flex-col justify-center break-words w-full">
-      <div className="text-center text-2xl pb-3 font-semibold">This Data fetch from Api</div>
+      <div className="text-center text-2xl pb-3 font-semibold">
+        This Data fetch from Api
+      </div>
       <div>{data.message}</div>
-      <div><span className="font-semibold">Your version is:</span> {data.version}</div>
-      <div><span className="font-semibold">Your token:</span>{data.token}</div>
+      <div>
+        <span className="font-semibold">Your version is:</span> {data.version}
+      </div>
+      <div>
+        <span className="font-semibold">Your token:</span>
+        {data.token}
+      </div>
     </div>
   );
 }
